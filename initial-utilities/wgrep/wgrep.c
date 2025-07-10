ebb10987ddfd2fde07ad21e9840a0d8d7c82ae7d
SWParkLaptopUbuntu <swpark.biz@gmail.com>
2025-07-09 19:25:24 -0700

    Add wgrep.c and passed all tests
    Used getline() so that the program can read very long lines.
    Used function strstr() which checks whether the certain string is contained to another string.

---
+++ initial-utilities/wgrep/wgrep.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void grep(FILE *fp, const char *search_term){
+  char *line =NULL;
+  size_t len=0;
+
+  while(getline(&line, &len, fp)!=-1){
+    if(strstr(line, search_term)!=NULL){ //strstr(line, serch_term) checks whether search_term is in the line
+      printf("%s",line);
+    }
+  }
+
+  free(line);
+}
+
+
+int main(int argc, char *argv[]){
+
+  if(argc<2){
+    printf("wgrep: searchterm [file ...]\n");
+    return 1;
+  }
+
+  const char *search_term = argv[1];
+    
+  if(argc==2){
+    grep(stdin, search_term);
+  }else{
+    for (int i=2; i<argc;i++){
+      FILE *fp = fopen(argv[i], "r");
+      if(fp==NULL){
+	printf("wgrep: cannot open file\n");
+	return 1;
+      }
+
+      grep(fp, search_term);
+      fclose(fp);
+    }
+  }
+    
+  return 0;
+}
